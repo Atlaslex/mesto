0823e31bcf826278da57195617eb2c8cea7ef97f
Atlaslex <atlaslex@yandex.ru>
2021-11-20 16:47:10 +0300

    spb_1.4

--- scripts/index.js
+++ scripts/index.js
@@ -3,19 +3,18 @@ console.log('loaded')
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
-const popupCloseOverlay = popup.querySelector('.popup__overlay');
+const popupCloseOverlay = popup.querySelector('.popup');
 
 function open() {
-    popup.classList.add('popap__opened');
-
+  popup.classList.add('popap_opened');
 }
 
 function close() {
-    popup.classList.remove('popap__opened');
+  popup.classList.remove('popap_opened');
 }
 editButton.addEventListener('click', open);
 popupCloseButton.addEventListener('click', close);
-popupCloseOverlay.addEventListener('click', close);
+
 
 let formElement = document.querySelector('.form');
 let formName = document.querySelector('.form__name');
@@ -24,17 +23,10 @@ let profileTitle = document.querySelector('.profile__cousteau');
 let profileSubtitle = document.querySelector('.profile__annotation');
 let submit = document.querySelector('.form__save');
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-
-    profileTitle.textContent = formName.value;
-    profileSubtitle.textContent = formVocation.value;
-    
-    // Получите значение полей jobInput и nameInput из свойства value
-
-    // Выберите элементы, куда должны быть вставлены значения полей
-
-    // Вставьте новые значения с помощью textContent
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+  profileTitle.textContent = formName.value;
+  profileSubtitle.textContent = formVocation.value;
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
