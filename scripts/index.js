f5d7d244254e6af695f488d72e2121208526773b
Atlaslex <atlaslex@yandex.ru>
2021-11-22 01:47:25 +0300

    spb_1.5

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,30 @@
-console.log('loaded')
-
 const editButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const popupCloseButton = popup.querySelector('.popup__close');
-const popupCloseOverlay = popup.querySelector('.popup');
+
+let formElement = document.querySelector('.form');
+let formName = document.querySelector('.form__item_type_name');
+let formVocation = document.querySelector('.form__item_type_vocation');
+let profileTitle = document.querySelector('.profile__cousteau');
+let profileSubtitle = document.querySelector('.profile__annotation');
 
 function open() {
   popup.classList.add('popap_opened');
+  formName.value = profileTitle.textContent;
+  formVocation.value = profileSubtitle.textContent;
 }
 
 function close() {
   popup.classList.remove('popap_opened');
 }
-editButton.addEventListener('click', open);
-popupCloseButton.addEventListener('click', close);
-
-
-let formElement = document.querySelector('.form');
-let formName = document.querySelector('.form__name');
-let formVocation = document.querySelector('.form__vocation');
-let profileTitle = document.querySelector('.profile__cousteau');
-let profileSubtitle = document.querySelector('.profile__annotation');
-let submit = document.querySelector('.form__save');
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   profileTitle.textContent = formName.value;
   profileSubtitle.textContent = formVocation.value;
+  close();
 }
 
+editButton.addEventListener('click', open);
+popupCloseButton.addEventListener('click', close);
 formElement.addEventListener('submit', formSubmitHandler);
-submit.addEventListener('click', close);
