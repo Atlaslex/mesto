991af1763e4e96de772596f9d067ba1853eb0dce
Atlaslex <atlaslex@yandex.ru>
2021-12-03 19:56:30 +0300

    feat: dynamic list

--- scripts/index.js
+++ scripts/index.js
@@ -28,3 +28,32 @@ function formSubmitHandler(evt) {
 editButton.addEventListener('click', open);
 popupCloseButton.addEventListener('click', close);
 formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+const initialCards = [
+  {
+    name: 'Кален Эмсли',
+    link: './blocks/element/__element-img/kalen-emsley.jpg'
+  },
+  {
+    name: 'Осло. Норвегия',
+    link: './blocks/element/__element-img/oslo.jpg'
+  },
+  {
+    name: 'Мадейра',
+    link: './blocks/element/__element-img/madeyra.jpg'
+  },
+  {
+    name: 'Италия',
+    link: './blocks/element/__element-img/italia.jpg'
+  },
+  {
+    name: 'Уитчемптон, Уимборн, Великобритания',
+    link: './blocks/element/__element-img/brit.jpg'
+  },
+  {
+    name: 'Исландия',
+    link: './blocks/element/__element-img/iceland.jpg'
+  }
+]; 
